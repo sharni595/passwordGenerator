2c27b518cd6e84d18813cd05986fe7a92395d5d9
Sharni Zaugg <sharni.greener@gmail.com>
2021-03-28 15:29:16 -0600

    add README content

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,6 @@
 // Assignment code here
-//create arrays containing each criteria
-//create for loop to cycle through arrays.
-//create function to push random characters to empty array
 
- 
-
-
- var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
  var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
  var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', ',', '{', '}', '|', ':', '>', '?', ';', '.'];
@@ -21,19 +15,14 @@ function generatePassword() {
     alert("Please select a valid number of characters.");
     return;
   }
-  console.log(passwordLength);
 
   var passwordNumbers = confirm("Do you want your password to include numbers?");
-  console.log(passwordNumbers);
 
   var passwordLowerCase = confirm("Do you want your password to include lowercase letters?");
-  console.log(passwordLowerCase);
 
   var passwordUpperCase = confirm("Do you want your password to include upper case letters?");
-  console.log(passwordUpperCase);
 
   var passwordSpecialChar = confirm("Do you want your password to include special characters?")
-  console.log(passwordSpecialChar);
  
   //if at least one is selected as true, continue with creating password. if not, create alert and run again.
   if (passwordNumbers === false &&
@@ -60,7 +49,6 @@ function generatePassword() {
         };
        return combineCharacters(passwordLength);
       }
-
 };
 
 // Get references to the #generate element
@@ -72,7 +60,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Password Generator Starter Code
+
+<!-- user story -->
+## User Story:
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+<!-- application description -->
+## Description
+
+This application was created to meet the users needs for higher security randomly generated passwords. As they follow the prompts to confirm what type of characters they would like included, a random password is generated with a combination of lowercase letters, uppercase letters, numbers, and special characters. 
+
+## Screenshots
+
+
+## Live URL
+
+https://sharni595.github.io/passwordGenerator/
